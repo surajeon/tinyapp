807fcb93c7f99be4321aa0938d0e7d6002eb1bdc
Sura Jeon <surajeon@gmail.com>
2021-05-10 20:14:57 +0000

    ejs installed and coded on express_server file

--- express_server.js
+++ express_server.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const morgan = require('morgan');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.set('veiw engine', 'ejs')
+
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -21,4 +20,17 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+
+ app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
