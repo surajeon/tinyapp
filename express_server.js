35f071ea5cb44694d10e98b4392b3e1fc2b7ba96
Sura Jeon <surajeon@gmail.com>
2021-05-13 05:51:07 +0000

    login, register, duplication check all done

--- express_server.js
+++ express_server.js
@@ -25,10 +25,20 @@ const urlDatabase = { // url database
 };
 
 const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
+  "test": {
+    id: "banana",
+    email: "b@b.com",
+    password: "1234"
+  },
+  "apple": {
+    id: "orange", 
     email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+    password: "1234"
+  },
+  "sura": {
+    id: "a",
+    email: "a@a.com",
+    password: "1234"
   }
 }
 
@@ -39,7 +49,17 @@ app.get("/", (req, res) => { // home
 });
 
 app.get("/urls", (req, res) => { // Load to My URLs page
-  const user = users[req.cookies["user_id"]];
+  const cookie = req.cookies["user_id"]
+  const userIdFunc = (cookie) => {
+    for (let user in users) {
+      if(users[user].id === cookie) {
+        return users[user];
+      }
+    }
+    return null;
+  }
+  const user = userIdFunc(cookie);
+
   const templateVars = { 
     urls: urlDatabase,
     user: user 
@@ -62,16 +82,37 @@ app.post("/urls/:shortURL", (req, res) => { // urls_show.ejs - display long URL 
 })
 
 app.get("/urls/new", (req, res) => { // urls_new - webpage to creat shortURL. it needs to be placed before shortURL get route. Because Express will think that new is a route parameter.  
-  const user = users[req.cookies["user_id"]];
+  const cookie = req.cookies["user_id"]
+  const userIdFunc = (cookie) => {
+    for (let user in users) {
+      if(users[user].id === cookie) {
+        return users[user];
+      }
+    }
+    return null;
+  }
+  const user = userIdFunc(cookie);
   const templateVars = { user: user }
   res.render("urls_new", templateVars);
 });
 //A good rule of thumb to follow is that routes should be ordered from most specific to least specific.
 
 app.get("/urls/:shortURL", (req, res) => { // urls_show.ejs - Retrieve the URL with shortURL that was created already.
+  const cookie = req.cookies["user_id"]
+  const userIdFunc = (cookie) => {
+    for (let user in users) {
+      if(users[user].id === cookie) {
+        return users[user];
+      }
+    }
+    return null;
+  }
+  const user = userIdFunc(cookie);
+
   const shortURL = req.params.shortURL;
-  const user = users[req.cookies["user_id"]];
+
   const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL], user: user };
+  res.cookie('user_id', user['id']);
   res.render("urls_show", templateVars);
 });
 
@@ -109,31 +150,6 @@ app.post("/urls/:shortURL", (req, res)=>{ // urls_index.ejs - edit button from m
 // in the /login get route we need to degign a page which asks for an email and pw, just like a registration
 // When the user submits the info, w eneed to verufy whetehr that user is existing or not with the password check
 // If the user is there and password is ok then we write the cookie, the way we wrote in regstration POST, and redierect it to the /urls
-app.get("/login",(req, res) => {
-  const user = users[req.cookies["user_id"]]; // user to userInfo?
-  const templateVars = { user: user };
-  res.render('urls_login', templateVars)
-});
-
-app.post("/login",(req,res) => { // _header.ejs - login and create cookies
-  // const userInfo = users[] // new random ID
-  const emailPassword = { userEmail: req.body.email, userPassword: req.body.password}
-  for (let userInfo in users) {
-    if (emailPassword[userEmail] === userInfo[email]) {
-      // email provided and email in "users" are the same 
-    } 
-  }
-  const userID = req.body.user;
-  // console.log(user);
-  res.cookie('username',username);
-  const user = req.body
-  res.redirect('/urls',users);
-});
-
-app.post("/logout", (req, res)=> { // _header.ejs - logout button 
-  res.clearCookie('user_id');
-  res.redirect('/urls');
-})
 
 app.get("/register", (req, res) => { // display registeration page
   const templateVars = { user: undefined};
@@ -142,11 +158,12 @@ app.get("/register", (req, res) => { // display registeration page
 
 app.post("/register", (req, res) => { // register email and password and redirect to /urls
   if (req.body.email === "" || req.body.password === "") { // email/password with empty string - 400 status code
-    res.send(400)
+    res.status(400)
+    res.end();
   } 
   let duplicateEmail = false; // On/Off switch?
   for (let userInfo in users) { // loop inside "users" to check if the email already exists
-    console.log(users[userInfo])
+    // console.log(users[userInfo])
     // console.log("user: ",user);
     // console.log("users[user]: ",users[user].email)
     if (req.body.email === users[userInfo].email) {
@@ -155,18 +172,53 @@ app.post("/register", (req, res) => { // register email and password and redirec
     } 
   }
   if (duplicateEmail === true) { // 
-    res.sendStatus(400);
+    res.status(400);
+    res.end();
   } else {  
     const rString = generateRandomString(6, chars);
     users[rString] = { id: rString, email: req.body.email, password: req.body.password }
-    // console.log("req.body: ", req.body) //  {email: newEmail , password: newPassword}
-    const usersInfo = users[rString];
-    // console.log(usersInfo);
-    res.cookie('user_id', usersInfo['id']);
+    res.cookie('user_id', rString); //ERROR --------------------------
     res.redirect('/urls');
+    res.end();
   }
 })
 
+app.get("/login",(req, res) => {
+  const user = users[req.cookies["user_id"]]; // user to userInfo?
+  const templateVars = { user: user };
+  res.render('urls_login', templateVars)
+});
+
+app.post("/login",(req,res) => { // _header.ejs - login and create cookies
+  console.log(users)
+  const email = req.body.email;
+  const password = req.body.password;
+  const userObj = (email, password) => {
+    for (let userInfo in users) {
+      if(users[userInfo].email === email && users[userInfo].password === password) {
+        return users[userInfo];
+      } 
+    }
+    return false;
+  }
+  const userData = userObj(email, password);
+  if(userData) {
+    const templateVars = { user: users, urls: urlDatabase };
+    res.cookie('user_id', userData.id);
+    res.render("urls_index", templateVars);
+    // res.redirect('/urls');
+    res.end();
+  } else {
+    res.status(400);
+    res.end();
+  }
+
+});
+
+app.post("/logout", (req, res)=> { // _header.ejs - logout button 
+  res.clearCookie('user_id');
+  res.redirect('/urls');
+})
 app.listen(PORT, () => { // my port 8080
   console.log(`Example app listening on port ${PORT}!`);
 });
