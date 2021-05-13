150aaeba410b0cf97d6d19dbb2f7dd55e02c97d6
Sura Jeon <surajeon@gmail.com>
2021-05-13 06:55:50 +0000

    complete

--- express_server.js
+++ express_server.js
@@ -1,227 +1,190 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+app.use(bodyParser.urlencoded({extended: true}));  //middleware
 app.use(cookieParser());
-// const morgan = require('morgan');
-
-function generateRandomString(length, arr) { // randomize shortURL function
-    var random = '';
-    for (var i = length; i > 0; i--) {
-        random += arr[Math.floor(Math.random() * arr.length)];
-    }
-    return random;
-}
-
-const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-const urlDatabase = { // url database
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+app.set('view engine', 'ejs');
+const generateRandomString = function(length, arr) {
+  let random = '';
+  for (let i = length; i > 0; i--) {
+    random += arr[Math.floor(Math.random() * arr.length)];
+  }
+  return random;
 };
-
-const users = { 
-  "test": {
-    id: "banana",
-    email: "b@b.com",
-    password: "1234"
-  },
-  "apple": {
-    id: "orange", 
-    email: "user@example.com", 
-    password: "1234"
-  },
-  "sura": {
-    id: "a",
-    email: "a@a.com",
-    password: "1234"
+const newRandomId = function(length, arr) {
+  let random = '';
+  for (let i = length; i > 0; i--) {
+    random += arr[Math.floor(Math.random() * arr.length)];
   }
-}
-
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => { // home
-  res.send("Hello!");
-});
-
-app.get("/urls", (req, res) => { // Load to My URLs page
-  const cookie = req.cookies["user_id"]
-  const userIdFunc = (cookie) => {
-    for (let user in users) {
-      if(users[user].id === cookie) {
-        return users[user];
-      }
-    }
-    return null;
+  return random;
+};
+const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const urlDatabase = {
+  'b2xVn2': { longURL: 'https://www.tsn.ca', userID: 'aJ48lW' },
+  '9sm5xK': { longURL: 'https://www.google.ca', userID: 'aJ48lW' }
+};
+const users = {
+  'userRandomID': {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur'
+  },
+  'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk'
   }
-  const user = userIdFunc(cookie);
-
-  const templateVars = { 
-    urls: urlDatabase,
-    user: user 
-  }; 
-  res.render("urls_index", templateVars);
+};
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
-
-app.post("/urls", (req, res) => { // new shortURL from /urls/new to /urls and redirect to /urls
-  const rString = generateRandomString(6, chars);
-  urlDatabase[rString] = req.body.longURL;
-  console.log(urlDatabase);
+app.post('/urls', (req, res) => {
+  const newString = generateRandomString(6, chars);
+  urlDatabase[newString] = {longURL: req.body.longURL, userID: req.cookies['user_id']};
+  // res.send('go to a new page');
   res.redirect('/urls');
 });
-
-app.post("/urls/:shortURL", (req, res) => { // urls_show.ejs - display long URL and Short URL created.
-  const newLongURL = req.body.longURL;
-  const shortURL = req.params.shortURL;
-  urlDatabase[shortURL] = newLongURL;
-  res.redirect("/urls");
-})
-
-app.get("/urls/new", (req, res) => { // urls_new - webpage to creat shortURL. it needs to be placed before shortURL get route. Because Express will think that new is a route parameter.  
-  const cookie = req.cookies["user_id"]
-  const userIdFunc = (cookie) => {
-    for (let user in users) {
-      if(users[user].id === cookie) {
-        return users[user];
-      }
-    }
-    return null;
-  }
-  const user = userIdFunc(cookie);
-  const templateVars = { user: user }
-  res.render("urls_new", templateVars);
-});
-//A good rule of thumb to follow is that routes should be ordered from most specific to least specific.
-
-app.get("/urls/:shortURL", (req, res) => { // urls_show.ejs - Retrieve the URL with shortURL that was created already.
-  const cookie = req.cookies["user_id"]
-  const userIdFunc = (cookie) => {
-    for (let user in users) {
-      if(users[user].id === cookie) {
-        return users[user];
-      }
-    }
-    return null;
-  }
-  const user = userIdFunc(cookie);
-
-  const shortURL = req.params.shortURL;
-
-  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL], user: user };
-  res.cookie('user_id', user['id']);
-  res.render("urls_show", templateVars);
-});
-
-app.get("/urls.json", (req, res) => { // JSON file
+// root path '/'; JSON string representing the entire urlDatabase object can see
+// '/sth' --> route
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
-
-app.get("/hello", (req, res) => { // localhost:8080/hello => Hello World
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (req, res) => {
+  // const templateVars = { greeting: 'Hello World!' };
+  // res.render('hello_world', templateVars);
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
-
-
-app.get("/u/:shortURL", (req, res) => { // direct to longURL(actual website)
-  const shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
+app.get('/urls', (req, res) => {
+  let userId = req.cookies['user_id'];
+  // console.log('users', users);
+  // console.log(users[userId]);
+  const templateVars = {
+    urls: urlsForUser(userId),
+    user: users[userId],
+    error: users[userId] ? null : 'Please Login or Register first'
+  };
+  // console.log('error', userId);
+  res.render('urls_index', templateVars);
 });
-
-
-app.post("/urls/:shortURL/delete", (req, res) => { // urls_index.ejs - delete button from my URLs
-  const shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
-  res.redirect("/urls");
-});
-
-app.post("/urls/:shortURL", (req, res)=>{ // urls_index.ejs - edit button from my URLs
-  const shortURL = req.params.shortURL;
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect("/urls");
-  // const longURL = urlDatabase[shortURL];
-  // res.redirect(longURL);  
-});
-
-// /login - it would go to the /login GET route.
-// in the /login get route we need to degign a page which asks for an email and pw, just like a registration
-// When the user submits the info, w eneed to verufy whetehr that user is existing or not with the password check
-// If the user is there and password is ok then we write the cookie, the way we wrote in regstration POST, and redierect it to the /urls
-
-app.get("/register", (req, res) => { // display registeration page
-  const templateVars = { user: undefined};
-  res.render('urls_register',templateVars)
-})
-
-app.post("/register", (req, res) => { // register email and password and redirect to /urls
-  if (req.body.email === "" || req.body.password === "") { // email/password with empty string - 400 status code
-    res.status(400)
-    res.end();
-  } 
-  let duplicateEmail = false; // On/Off switch?
-  for (let userInfo in users) { // loop inside "users" to check if the email already exists
-    // console.log(users[userInfo])
-    // console.log("user: ",user);
-    // console.log("users[user]: ",users[user].email)
-    if (req.body.email === users[userInfo].email) {
-      // console.log("New email: ",req.body.email)
-      duplicateEmail = true;
-    } 
+const urlsForUser = function(id) {
+  let result = {};
+  for (let shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      result[shortURL] = urlDatabase[shortURL];
+    }
   }
-  if (duplicateEmail === true) { // 
-    res.status(400);
-    res.end();
-  } else {  
-    const rString = generateRandomString(6, chars);
-    users[rString] = { id: rString, email: req.body.email, password: req.body.password }
-    res.cookie('user_id', rString); //ERROR --------------------------
+  return result;
+};
+// if we place this route after the /urls/:id definition, any calls to /urls/new will be handled by app.get('/urls/:id', ...) because Express will think that new is a route parameter.
+app.get('/urls/new', (req, res) => {
+  let userId = req.cookies['user_id'];
+  const templateVars = { user: users[userId] };
+  if (userId) {
+    res.render('urls_new', templateVars);
+  } else {
+    res.redirect('/login');
+  }
+});
+app.get('/urls/:shortURL', (req, res) => {
+  let userId = req.cookies['user_id'];
+  const shortURL = req.params.shortURL;
+  const templateVars = {
+    shortURL: shortURL,
+    longURL: urlDatabase[shortURL].longURL,
+    user: users[userId],
+    error: users[userId] ? null : 'Please Login or Register first' };
+  if (userId) {
+    res.render('urls_show', templateVars);
+  } else {
     res.redirect('/urls');
-    res.end();
   }
-})
-
-app.get("/login",(req, res) => {
-  const user = users[req.cookies["user_id"]]; // user to userInfo?
-  const templateVars = { user: user };
-  res.render('urls_login', templateVars)
 });
-
-app.post("/login",(req,res) => { // _header.ejs - login and create cookies
-  console.log(users)
+app.get('/login', (req, res) => {
+  let userId = req.cookies['user_id'];
+  const templateVars = {user: users[userId]};
+  res.render('urls_login', templateVars);
+});
+app.get('/register', (req, res) => {
+  const templateVars = {user: null};
+  res.render('urls_register', templateVars);
+});
+app.post('/urls/:shortURL/delete', (req, res) => {
+  let userId = req.cookies['user_id'];
+  if (!users[userId]) {
+    res.status(403).send('Not Found');
+  } else {
+    const deleteToggle = req.params.shortURL;
+    delete urlDatabase[deleteToggle];
+    res.redirect('/urls');
+  }
+});
+app.post('/urls/:shortURL', (req, res) => {
+  let userId = req.cookies['user_id'];
+  if (!users[userId]) {
+    res.status(403).send('Not Found');
+  } else {
+    const shortURL = req.params.shortURL;
+    const newLongURL = req.body.longURL;
+    urlDatabase[shortURL].longURL = newLongURL;
+    res.redirect('/urls');
+  }
+});
+const findUserIdByEmail = function(email) {
+  for (let user in users) {
+    if (users[user].email === email) {
+      return users[user].id;
+    }
+  }
+  return null;
+};
+app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const userObj = (email, password) => {
-    for (let userInfo in users) {
-      if(users[userInfo].email === email && users[userInfo].password === password) {
-        return users[userInfo];
-      } 
-    }
-    return false;
+  const userId = findUserIdByEmail(email);
+  if (!userId) {
+    res.status(403).send('Not Found');
   }
-  const userData = userObj(email, password);
-  if(userData) {
-    const templateVars = { user: users, urls: urlDatabase };
-    res.cookie('user_id', userData.id);
-    res.render("urls_index", templateVars);
-    // res.redirect('/urls');
-    res.end();
+  if (users[userId] && password === users[userId].password) {
+    res.cookie('user_id', userId);
   } else {
-    res.status(400);
-    res.end();
+    res.status(403).send('Forbidden');
   }
-
+  res.redirect('/urls');
 });
-
-app.post("/logout", (req, res)=> { // _header.ejs - logout button 
+app.post('/logout', (req, res) => {
   res.clearCookie('user_id');
   res.redirect('/urls');
-})
-app.listen(PORT, () => { // my port 8080
+});
+app.post('/register', (req, res) => {
+  const userId = newRandomId(6, chars);
+  // console.log(users);
+  if (req.body.email === '' || req.body.password === '' || findExistingEmail(req.body.email)) {
+    const status = 400;
+    res
+      .status(status)
+      .send('error');
+  } else {
+    users[userId] = {
+      id: userId,
+      email: req.body.email,
+      password: req.body.password
+    };
+  }
+  res.cookie('user_id', userId);
+  res.redirect('/urls');
+});
+// true, false
+const findExistingEmail = function(email) {
+  for (let user in users) {
+    // console.log('user', users[user])
+    if (users[user].email === email) {
+      return true;
+    }
+  }
+  return false;
+};
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-
